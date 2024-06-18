395951a95cd3737ecacd09539a9b198dcd46e287
shubat <fa.alshubat@gmail.com>
2024-06-18 08:35:20 +0300

    upload game computer choice

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello World")
+const game = {rock:"rock" , paper:"paper" , scissors:"scissor"};
+
+const gameChoices = Object.keys(game);
+
+function getComputerChoice() {
+
+let randomChoice = Math.floor(Math.random() * gameChoices.length)    
+return gameChoices[randomChoice];
+
+
+}
+
+
+const computerChoice = getComputerChoice();
+console.log(computerChoice);
