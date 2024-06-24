db7808d170e6bafb65889a69fb260c58295f3825
shubat <fa.alshubat@gmail.com>
2024-06-24 18:35:42 +0300

    making the logic of the game'

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-const game = {rock:"rock" , paper:"paper" , scissors:"scissor"};
+const game = {rock:"scissor", paper:"rock" , scissors:"paper"}
 
 const gameChoices = Object.keys(game);
+
+
+
+
+
 
 function getComputerChoice() {
 
@@ -11,5 +16,36 @@ return gameChoices[randomChoice];
 }
 
 
+
+
+function getHumanChoice() {
+    let pickChoice = prompt("choose from rock, papar or scissor").toLowerCase();
+    return pickChoice;
+}
+
+
+const humanChoice = getHumanChoice();
 const computerChoice = getComputerChoice();
+console.log(humanChoice);
 console.log(computerChoice);
+
+function playgame() {
+
+
+if (humanChoice == computerChoice){
+return 'it is a tie';}
+
+    if (game[humanChoice] === computerChoice) {
+    return 'you win';}
+    
+    else {
+        return 'computer wins'
+    }
+
+    }
+
+
+
+
+const result = playgame();
+console.log(result);
